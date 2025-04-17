4036aa057f6c5b9b9a51e80deab03d2bc24b1098
FedoRich-P <panchev_fedor@mail.ru>
2025-04-17 20:07:14 +0300

    s

--- src/shared/lib/route/parseBreadcrumbs.ts
+++ src/shared/lib/route/parseBreadcrumbs.ts
@@ -6,10 +6,3 @@ export const parseBreadcrumbs = (pathname: string) => {
         path: "/" + pathParts.slice(0, index + 1).join("/"),
     }));
 };
-
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:FedoRich-P/users-list-router-dom.git
-git push -u origin main
